c253e940fd35515936e396bf1dd7828d62d072b7
Dark Angel <DarkAngel@darkangel-macbook-pro-2.local>
2017-08-01 16:54:01 +0800

    Fix bugs

--- UINavigationController+DAPowerfulCustomization/UINavigationController+DAPowerfulCustomization.m
+++ UINavigationController+DAPowerfulCustomization/UINavigationController+DAPowerfulCustomization.m
@@ -371,7 +371,7 @@ static inline CGFloat da_calculateMedianValue(CGFloat a, CGFloat b, CGFloat perc
     // Call the default implementation
     [self da_viewDidLoad];
     // Update the bar appearance
-    if ([UIDevice currentDevice].systemVersion.floatValue < 10) {
+    if ([UIDevice currentDevice].systemVersion.floatValue < 11) {
         dispatch_async(dispatch_get_main_queue(), ^{
             [self da_updateNavigationBarAndStatusBarAppearance];
         });
