83acd70a8c7fb33067b915bfef8c5ec4cf2d9ba1
Dark Angel <DarkAngel@darkangel-macbook-pro-2.local>
2017-10-23 17:00:43 +0800

    Fix bugs on iOS 11 when backgroundImage and searchBar both set on navigationBar.

--- UINavigationController+DAPowerfulCustomization/UINavigationController+DAPowerfulCustomization.m
+++ UINavigationController+DAPowerfulCustomization/UINavigationController+DAPowerfulCustomization.m
@@ -624,8 +624,10 @@ static inline CGFloat da_calculateMedianValue(CGFloat a, CGFloat b, CGFloat perc
         }
     }
     self.navigationBar.titleTextAttributes = navigationItem.da_navigationBarTitleTextAttributes;
-    CGFloat statusBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
-    [self da_fakeBackgroundImageView].frame = CGRectMake(0, 0, self.navigationBar.frame.size.width, self.navigationBar.frame.size.height + statusBarHeight);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIImageView *backgroundImgView = [backgroundView valueForKey:@"backgroundImageView"];
+        [self da_fakeBackgroundImageView].frame = backgroundImgView.frame;
+    });
 }
 
 /**
